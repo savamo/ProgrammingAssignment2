8666f9e0ccafc876715f65cc4f157a4ecb60d40b
savamo <savio.amodio@gmail.com>
2014-04-25 01:37:34 +0200

    Final release of functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,28 @@
 
 ## Write a short comment describing this function
 
+## example of usage:
+## > mmm <- replicate(20, rnorm(20))
+## > cm <- makeCacheMatrix(mmm)
+## > xx <- cacheSolve(cm) # getting NOT cached data
+## > xx <- cacheSolve(cm) # getting cached data
+
+
 makeCacheMatrix <- function(x = matrix()) {
 
-  xx <- 33
+  # random matrix: replicate(20, rnorm(20))
+   
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve  <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve )
   
 }
 
@@ -13,5 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
+  
 }
